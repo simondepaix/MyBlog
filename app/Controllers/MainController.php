64b9162553e9b63a63d69ad040dc797cafdfe08f
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-07-29 12:51:23 +0200

    correction partie 4

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,11 @@
+<?php
+class MainController{
+
+    public function render(string $view, array $data = []){                 
+
+        require __DIR__.'/../views/front/layouts/header.phtml';
+        require __DIR__."/../views/front/partials/$view.phtml";
+        require __DIR__.'/../views/front/layouts/footer.phtml';
+        
+    }
+}
