db4a20423228d25e7d3f2b6028d1a9468179e78b
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-08-01 10:13:56 +0200

    Update MainController.php

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,11 +1,50 @@
 <?php
 class MainController{
-
-    public function render(string $view, array $data = []){                 
-
+    protected $view;    
+    protected $data;
+    public function render(){             
+        $data = $this->data;                              
         require __DIR__.'/../views/front/layouts/header.phtml';
-        require __DIR__."/../views/front/partials/$view.phtml";
+        require __DIR__."/../views/front/partials/$this->view.phtml";
         require __DIR__.'/../views/front/layouts/footer.phtml';
         
+    }
+
+    /**
+     * Get the value of view
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * Set the value of view
+     */
+    public function setView($view): self
+    {
+        $this->view = $view;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get the value of data
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set the value of data
+     */
+    public function setData($data): self
+    {
+        $this->data = $data;
+
+        return $this;
     }
 }
