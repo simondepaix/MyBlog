46b8bd91b1ca84070444b2947e2376126b126bc5
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-08-01 12:33:55 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-$base_uri = $_SERVER['REQUEST_URI'];
+
 require '../app/views/front/layouts/header.phtml';
 require '../app/views/front/partials/index.phtml';
 require '../app/views/front/layouts/footer.phtml';
