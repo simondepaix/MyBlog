acf72940e170be40cc33ae846135399b54c69501
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-08-03 16:51:51 +0200

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,9 +3,6 @@
 class HomeController extends MainController{
 
     public function renderHome(){    
-        require __DIR__.'/../Models/PostModel.php';
-        $postModel = new PostModel();        
-        $this->data = $postModel->getPosts(5);   
         $this->render();
     }
 }
