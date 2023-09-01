ce0ff99c03c04bb41e39d28c7ab49e222cfd7d44
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-09-01 14:08:30 +0200

    Update PostModel.php

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -19,7 +19,7 @@ class PostModel
     private $user_id;
 
     // méthode pour récupérer tous les articles, il est possible de spécifier une limite
-    public static function getPosts(int $limit = null): ?PostModel
+    public static function getPosts(int $limit = null): ?array
     {
         // connexion pdo avec le pattern singleton
         $pdo = DataBase::connectPDO();
